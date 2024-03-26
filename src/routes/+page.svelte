0c8473238ba72c839e534ae6cf45c540722b907e
pabl-o-ce <pabloce@poscye.com>
2024-03-25 19:10:11 -0500

    style: 🧚‍♀️ svelte is amazing max

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import "../app.scss";
+  import Chat from '../components/chat.svelte'
+</script>
+
+<main class="bg-slate-300">
+    <Chat></Chat>
+</main>
